65a9cde0089bacb6fa700d5e4c14931086e84f91
Rajalakshmi05 <105733739+Rajalakshmi05@users.noreply.github.com>
2022-05-24 14:54:21 -0400

    Add files via upload

---
+++ LearnEdit.java
@@ -0,0 +1,47 @@
+package Week2Day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnEdit {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		  ChromeDriver driver=new ChromeDriver();
+		  driver.get("http://leaftaps.com/opentaps/control/main");
+		  driver.manage().window().maximize();
+		  WebElement eleUserName = driver .findElement(By.id("username"));
+			eleUserName.sendKeys("DemoSalesManager");
+			driver.findElement(By.id("password")).sendKeys("crmsfa");
+			driver.findElement(By.className("decorativeSubmit")).click();
+			driver.findElement(By.linkText("CRM/SFA")).click();
+			driver.findElement(By.linkText("Leads")).click();
+			driver.findElement(By.linkText("Create Lead")).click();
+		  driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+			driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Rajalakshmi");
+			driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Mahalingam");
+			WebElement ele = driver.findElement(By.id("createLeadForm_dataSourceId"));
+			Select dd = new Select(ele);
+			dd.selectByVisibleText("Conference");
+			driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Raji");
+			driver.findElement(By.id("createLeadForm_lastNameLocal")).sendKeys("Maha");
+			driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("CSE");
+			driver.findElement(By.id("createLeadForm_description")).sendKeys("Amazing leading Company");
+			driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("raji5.maha@gmail.com");
+			WebElement state = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+			Select wb = new Select(state);
+			wb.selectByVisibleText("New York");
+			driver.findElement(By.className("smallSubmit")).click();
+			driver.findElement(By.linkText("Edit")).click();
+			driver.findElement(By.id("updateLeadForm_description")).clear();
+			driver.findElement(By.id("updateLeadForm_importantNote")).sendKeys("Automation is Amazing");
+			driver.findElement(By.className("smallSubmit")).click();
+			System.out.println("Title of Web Page is :"+driver.getTitle());
+		
+	}
+
+}
